464658e312089691e33cfc3c8a0c1c00e43905e7
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2017-12-30 12:19:59 +0300

    Исправлены замечания Валл Ли

--- bars.py
+++ bars.py
@@ -10,56 +10,52 @@ def load_data(filepath):
 
 
 data = load_data(file)
-x = len(data['features'])
+data_length = len(data['features'])
 
 
-def get_name_and_seatscount():
-    '''Function to create new dictionary with data: {'bar_name': number_of_seats}'''
-    n_dict = []
-    for i in range(x):
-        name = data['features'][i]['properties']['Attributes']['Name']
-        seatscount = data['features'][i]['properties']['Attributes']['SeatsCount']
+def get_bar_name_and_seatscount():
+    new_dict = []
+    for index in range(data_length):
+        bar_name = data['features'][index]['properties']['Attributes']['Name']
+        seatscount = data['features'][index]['properties']['Attributes']['SeatsCount']
         if seatscount != 0: #append to the list only the bars which have at least one seat
-            n_dict.append([name, seatscount])
-    return dict(n_dict)
+            new_dict.append([bar_name, seatscount])
+    return dict(new_dict)
 
 
-new_data = get_name_and_seatscount()
-
+new_data = get_bar_name_and_seatscount()
 
 def get_biggest_bar():
-    '''Function to print the biggest bar by the number of seats'''
     max_seats = max(new_data.values())
     key = list(new_data.keys())[list(new_data.values()).index(max_seats)]
     print('Самый большой бар: ' + key)
 
 
 def get_smallest_bar():
-    '''Function to print the smallest bar according to the number of seats'''
     min_seats = min(new_data.values())
     key = list(new_data.keys())[list(new_data.values()).index(min_seats)]
     print('Самый маленький бар: ' + key)
 
 
-def get_name_and_coords():
-    '''Function to create new dictionary with data {'bar_name':coordinates}'''
+def get_bar_name_and_coordinates():
     coords = []
-    for i in range(x):
-        name = data['features'][i]['properties']['Attributes']['Name']
-        geometry = data['features'][i]['geometry']['coordinates']
-        coords.append([name, geometry])
+    for index in range(data_length):
+        bar_name = data['features'][index]['properties']['Attributes']['Name']
+        geometry = data['features'][index]['geometry']['coordinates']
+        coords.append([bar_name, geometry])
     return dict(coords)
 
 
-def get_closest_bar(longitude, latitude):
-    '''Function to print the closest bar'''
-    coords = get_name_and_coords()
-    dist = lambda key: ((longitude - coords[key][0]) ** 2 + (latitude - coords[key][1]) ** 2)
-    found = min(coords, key=dist)
+def get_closest_bar():
+    longitude = float(input('Долгота: '))
+    latitude = float(input('Широта: '))
+    coords = get_bar_name_and_coordinates()
+    distance = lambda key: ((longitude - coords[key][0]) ** 2 + (latitude - coords[key][1]) ** 2)
+    found = min(coords, key=distance)
     print('Ближайший бар: ' + found)
 
 
 if __name__ == '__main__':
     get_biggest_bar()
     get_smallest_bar()
-    get_closest_bar(37.492603324522143, 55.827437174064421)
+    get_closest_bar()
