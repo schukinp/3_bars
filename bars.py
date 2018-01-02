6ed2b04a47e3edef8ee358d45d05ee7daec43b81
Pavel Shchukin <30902662+schukinp@users.noreply.github.com>
2018-01-03 00:39:35 +0300

    оптимизирован скрипт и ридми

--- bars.py
+++ bars.py
@@ -1,47 +1,36 @@
 import json
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-file')
+args = parser.parse_args()
+filepath = args.file
 
 
 def load_data(filepath):
-    with open(filepath, 'r', encoding='utf8') as file:
+    with open(filepath, 'r', encoding='utf-8') as file:
         return json.load(file)
 
 
-def get_bar_name_coordinates_and_seats():
-    filtered_file = []
-    for element in load_data(filepath)['features']:
-        coords = element['geometry']['coordinates']
-        bar_name = element['properties']['Attributes']['Name']
-        seats = element['properties']['Attributes']['SeatsCount']
-        if seats > 0:
-            filtered_file.append([coords, bar_name, seats])
-    return filtered_file
+bar_list = load_data(filepath)['features']
 
 
 def get_biggest_bar():
-    return tuple(max(get_bar_name_coordinates_and_seats(), key=lambda el: el[2])[1:3])
+    return max(bar_list, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_smallest_bar():
-    return tuple(min(get_bar_name_coordinates_and_seats(), key=lambda el: el[2])[1:3])
-
-
-def get_closest_coords(longitude, latitude, element):
-    distance = (longitude - element[0][0]) ** 2 + (latitude - element[0][1]) ** 2
-    return distance
+    return min(bar_list, key=lambda bar: bar['properties']['Attributes']['SeatsCount'])
 
 
 def get_closest_bar(longitude, latitude):
-    return min(get_bar_name_coordinates_and_seats(), key=lambda el: get_closest_coords(longitude, latitude, el))[1]
-
-
-filepath = input('Введите путь к файлу (например, c:/path/file.json): ')
-longitude = float(input('Введите значение долготы: '))
-latitude = float(input('Введите значение ширины: '))
-print('Самый большой бар: %s с количеством мест: %d' % get_biggest_bar())
-print('Cамый маленький бар: %s с количество мест: %d' % get_smallest_bar())
-print('Ближайший бар: \'%s\'' % get_closest_bar(longitude, latitude))
+    return min(bar_list, key=lambda bar: (longitude - bar['geometry']['coordinates'][0]) ** 2 -
+                                         (latitude - bar['geometry']['coordinates'][1]) ** 2)
 
 
 if __name__ == '__main__':
-    pass
-
+    longitude = float(input('Введите значение долготы: '))
+    latitude = float(input('Введите значение ширины: '))
+    print('Самый большой бар: {}'.format(get_biggest_bar()['properties']['Attributes']['Name']))
+    print('Cамый маленький бар: {}'.format(get_smallest_bar()['properties']['Attributes']['Name']))
+    print('Ближайший бар: {}'.format(get_closest_bar(longitude, latitude)['properties']['Attributes']['Name']))
